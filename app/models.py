3506e1fac0f80f1c2e3c27561ea49794e6a77d1b
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-23 23:43:16 +0100

    removed unnecessary stuff

--- app/models.py
+++ app/models.py
@@ -1,23 +1,10 @@
 from crud import Model
 from pyrepositories import Entity
-from typing import List, Optional
-
-
-class Organizer(Model):
-    email: str
-
-
-class Joiner(Model):
-    name: str
-    company: str
-
 
 class Event(Model):
     date: str
-    organizer: Organizer
     status: str
     max_attendees: int
-    joiners: Optional[List[Joiner]] = None
 
 
 class EventEntity(Entity):
@@ -27,12 +14,6 @@ class EventEntity(Entity):
     @date.setter
     def date(self, value):
         self.fields["date"] = value
-    @property
-    def organizer(self):
-        return self.fields.get("organizer")
-    @organizer.setter
-    def organizer(self, value):
-        self.fields["organizer"] = value
     @property
     def status(self):
         return self.fields.get("status")
@@ -45,9 +26,3 @@ class EventEntity(Entity):
     @max_attendees.setter
     def max_attendees(self, value):
         self.fields["max_attendees"] = value
-    @property
-    def joiners(self):
-        return self.fields.get("joiners") or []
-    @joiners.setter
-    def joiners(self, value):
-        self.fields["joiners"] = value
